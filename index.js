616f2f36eef5ab278914a81ddbb4604d69940df2
Yu Yan <miracleyanyu@gmail.com>
2020-04-18 12:56:27 -0700

    fix cross origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ require('./data/db');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin",
-      "http://localhost:3001");
+      "*");
   res.header("Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods",
