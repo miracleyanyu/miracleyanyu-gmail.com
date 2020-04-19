a8a3aab8d3a2c7e15205a5a76437eeaf3976e69b
Yu Yan <miracleyanyu@gmail.com>
2020-04-18 17:51:23 -0700

    fix CORS: add put, delete, post

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, DELETE, POST');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
   return next();
