e716ad52a4488eff18f8f533bdb6678b7a8b1f45
Yu Yan <miracleyanyu@gmail.com>
2020-04-19 22:20:25 -0700

    fix CORS

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 require('./data/db');
 
 app.use(function(req, res, next) {
-  var allowedOrigins = ['https://cs5610-sp20-team18.herokuapp.com', 'http://localhost:4200', 'http://127.0.0.1:9000', 'http://localhost:9000', 'http://localhost:3000', 'http://localhost:3001'];
+  var allowedOrigins = ['https://cs5610-sp20-team18.herokuapp.com', 'https://cs5610-sp20-team18.herokuapp.com', 'http://localhost:4200', 'http://127.0.0.1:9000', 'http://localhost:9000', 'http://localhost:3000', 'http://localhost:3001'];
   var origin = req.headers.origin;
   if(allowedOrigins.indexOf(origin) > -1){
     res.header('Access-Control-Allow-Origin', origin);
